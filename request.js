e231f0df49b93dfc97f80c1d4e518b8395eb7b52
b5er <bservia@gmail.com>
2018-12-01 15:08:05 -0500

    restructuring of sky and ground

--- request.js
+++
@@ -1,9 +0,0 @@
-
-const smartcar = require('smartcar');
-
-const accessToken = '7a374360-2f27-4023-896d-0ea9dc50c629';
-
-smartcar.getVehicleIds(accessToken)
-    .then(function(response) {
-        console.log(response);
-    });
