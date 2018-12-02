60db4e29c6fd70e962f565c96173e29f8887747e
Hugo Perdomo <hugo@Hugos-Mac18.local>
2018-12-01 20:58:38 -0500

    ...

--- ryd_share_sky/src/components/landing/Hero.js
+++ ryd_share_sky/src/components/landing/Hero.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 
 import HeroImage from '../../assets/img/hero_image.svg'
+import SmartCar from '../../assets/img/smartcar.svg'
 
 class Hero extends Component {
 	render() {
 		return (
-			<section className="hero is-fullheight-with-navbar is-primary is-bold">
+			<section className="hero is-fullheight is-primary is-bold">
 	            <nav className="navbar is-primary is-transparent no-shadow" role="navigation" aria-label="main navigation">
 	                <div className="container">
 	                    <div className="navbar-brand">
@@ -13,13 +14,13 @@ class Hero extends Component {
 	                            {/*<img src={HeroImage} alt="" width="112" height="28" />*/}
 	                        </a>
 	                        <a className="navbar-item is-hidden-desktop is-hidden-tablet" href="avascript:void(0)">
-	                            <div id="menu-icon-wrapper" className="menu-icon-wrapper" style={{visibility: 'visible'}}>
+	                            <div id="menu-icon-wrapper" className="menu-icon-wrapper" style={{visibility: 'hidden'}}>
 	                                <svg width="1000px" height="1000px">
 	                                    <path className="path1" d="M 300 400 L 700 400 C 900 400 900 750 600 850 A 400 400 0 0 1 200 200 L 800 800"></path>
 	                                    <path className="path2" d="M 300 500 L 700 500"></path>
 	                                    <path className="path3" d="M 700 600 L 300 600 C 100 600 100 200 400 150 A 400 380 0 1 1 200 800 L 800 200"></path>
 	                                </svg>
-	                                <button id="menu-icon-trigger" className="menu-icon-trigger" />
+	                                <button id="menu-icon-trigger" className="menu-icon-trigger"/>
 	                            </div>
 	                        </a>
 	            
@@ -70,7 +71,7 @@ class Hero extends Component {
 	                    <div className="columns is-vcentered">
                             <div className="column is-5 is-offset-1">
                                 <figure className="image is-4by3">
-                                    <img src={HeroImage} alt="Description" style={{transform: "scale(2.4)"}} />
+                                    <img src={HeroImage} alt="Description" style={{transform: "scale(1.8)"}} />
                                 </figure>
                             </div>
 	                        <div className="column is-3 is-offset-1 landing-caption">
@@ -82,11 +83,24 @@ class Hero extends Component {
                                 <div className="field">
                                     <div className="control"><input type="password" className="input is-rounded" placeholder="Password"/></div>
                                 </div>
-	                            <div className="field">
-                                    <div className="control">
-                                        <button className="button is-success is-outlined glow is-rounded is-medium">Log In</button>
-                                    </div>
+
+								<div className="columns is-centered" style={{"padding-top": "40px"}}>
+									<div className="column is-5">
+                                        <div className="field">
+                                            <div className="control">
+                                                <button className="button is-success is-outlined glow is-rounded is-medium">Log In</button>
+                                            </div>
+                                        </div>
+									</div>
 								</div>
+
+								<p className="has-text-centered">Don't have an account yet?</p>
+                                <div className="columns is-centered">
+									<div className="column is-4">
+                                        <a href="/" className="">Sign up</a>
+									</div>
+								</div>
+
 	                        </div>
 
 	                    </div>
@@ -95,7 +109,11 @@ class Hero extends Component {
 
 	            <div className="hero-foot mb-20">
 	                <div className="container">
-	                    <p className="title">Powered by</p>
+	                    <p className="title">Powered by
+                            <img src={SmartCar} alt=""/>
+						</p>
+
+
 	                </div>
 	            </div>
 	        </section>
