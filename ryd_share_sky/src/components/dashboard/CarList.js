52ed7ca2e1ed2b42c4cc7dff72d3209490f62f57
sougatadafader <sougatadafader@gmail.com>
2018-12-02 09:15:29 -0500

    ..

--- ryd_share_sky/src/components/dashboard/CarList.js
+++ ryd_share_sky/src/components/dashboard/CarList.js
@@ -6,11 +6,60 @@ class CarList extends Component {
 	render() {
 		return (
 			<div>
-				<div className="columns">
+				<div className="columns ">
+
+                    <div className="column is-4">
+                        <i className="fa fa-2x fa-car"></i>
+                        <button className="button is-rounded is-medium" style={{marginRight: '12em'}}>
+
+                            Open car
+                        </button>
+                        <button className="button is-rounded is-medium is-danger" style={{marginRight: '12em'}}>Close car
+                        </button>
+                    </div>
+                    <div className="column is-3 ">
+                        <i className="fa fa-2x fa-car"></i>
+                        <div className="field">
+                            <button className="button is-rounded is-medium" style={{marginRight: '12em'}}>
+                                Open car
+                            </button>
+						</div>
+                        <div className="field">
+                            <button className="button is-rounded is-medium is-danger" style={{marginRight: '12em'}}>Close car
+                            </button>
+						</div>
+
+
+					</div>
+                    <div className="column is-3">
+                        <i className="fa fa-2x fa-car"></i>
+                        <div className="field">
+                            <button className="button is-rounded is-medium" style={{marginRight: '12em'}}>
+
+                                Open car
+                            </button>
+						</div>
+                        <div className="field">
+                            <button className="button is-rounded is-medium is-danger" style={{marginRight: '12em'}}>Close car
+                            </button>
+						</div>
+
+
+					</div>
+                    <div className="column is-3">
+                        <i className="fa fa-2x fa-car"></i>
+                        <div className="field">
+                            <button className="button is-rounded is-medium" style={{marginRight: '12em'}}>Open car
+                            </button>
+						</div>
+                        <div className="field">
+                            <button className="button is-rounded is-medium is-danger" style={{marginRight: '12em'}}>Close car
+                            </button>
+						</div>
+
+					</div>
 					<div className="column is-offset-5">
-						<button className="button is-rounded is-medium" style={{marginRight: '12em'}}>
-							Open car
-						</button>
+
 
 						<button className="button is-rounded is-medium is-danger">
 							Close car
