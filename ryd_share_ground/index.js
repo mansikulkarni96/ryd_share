1745dfaaf8adf3db34a3fa9ab4192b974470925c
Hugo Perdomo <hugo@Hugos-Mac18.local>
2018-12-02 05:42:41 -0500

    initial commit

--- ryd_share_ground/index.js
+++ ryd_share_ground/index.js
@@ -3,12 +3,59 @@ require('dotenv').config();
 const   smartcar        = require('smartcar'),
         express         = require('express'),
         bodyParser      = require('body-parser'),
+        mongoose        = require('mongoose'),
+        passport        = require('passport'),
+        LocalStrategy   = require("passport-local"),
+        passportMon     = require('passport-local-mongoose'),
+    //Smart car
         clientId        = process.env.CLIENTID,
         clientSecret    = process.env.CLIENTSECRET,
         redirectUri     = process.env.REDIRECTURI,
+    //MongoDB
+        dbUser          = process.env.DB_USER,
+        dbPasswd        = process.env.DB_PASSWD,
+        dbPath          = process.env.DB_PATH,
         port            = 8000;
 
-let app = express();
+let     app             = express();
+mongoose.connect(`mongodb://${dbUser}:${dbPasswd}@${dbPath}`, {useNewUrlParser: true});
+/**
+ * User Schema
+ * username: an email
+ * accessToken: Whatever we get from Smart Car API
+ */
+const UserSchema = new mongoose.Schema({
+   fullName: String,
+    username: String,
+    password: String,
+    accessToken: String,
+});
+
+UserSchema.plugin(passportMon);
+// Schema Model
+const User = mongoose.model("User", UserSchema);
+
+
+
+/**
+ * Middleware
+ */
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: process.env.Secret,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Setting up Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//Serialize user
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Client setup
 const Client = new smartcar.AuthClient({
@@ -75,6 +122,35 @@ app.get('/vehicles/:token', (req, res) => {
         .then(function(response) {
             res.json(response);
         });
+
+});
+
+
+app.post('/register', (req, res) =>{
+
+    let username = req.body.username;
+    let fullName = req.body.fullName;
+    let password = req.body.password;
+    console.log(req.body);
+
+    User.register(new User({username, fullName}), password, (err, response) => {
+        if(err){
+            res.json({register: "failed"});
+        }
+        passport.authenticate("local")(req, res, () =>{
+            res.json({register: "success"});
+        });
+    });
+
+});
+
+
+/**
+ * Authenticate Here
+ */
+app.post('/auth', passport.authenticate('local'), (req, res) => {
+
+    res.json({login: "valid"});
 
 });
 
