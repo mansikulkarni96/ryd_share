03d881007b80d0c796ef445256eaa99baf505208
Hugo Perdomo <hugo@Hugos-Mac18.local>
2018-12-01 19:01:38 -0500

    fixing background image

--- ryd_share_sky/src/components/landing/Hero.js
+++ ryd_share_sky/src/components/landing/Hero.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 class Hero extends Component {
 	render() {
 		return (
-			<section className="hero is-fullheight-with-navbar login-path is-bold">
+			<section className="hero is-fullheight login-path is-bold">
 	            <nav className="navbar is-primary is-transparent no-shadow" role="navigation" aria-label="main navigation">
 	                <div className="container">
 	                    <div className="navbar-brand">
@@ -84,23 +84,8 @@ class Hero extends Component {
 
 	            <div className="hero-body">
 	                <div className="container">
-	                    <div className="columns is-vcentered">
-	                        <div className="column is-5 is-offset-1 landing-caption">
-	                            <h1 className="title is-1 is-bold is-spaced">
-	                                Manage, Deploy.
-	                            </h1>
-	                            <h2 className="subtitle is-5 is-muted">Lorem ipsum sit dolor amet is a dummy text used by typography industry </h2>
-	                            <p>
-	                                <a className="button cta rounded primary-btn raised">
-	                                    Get Started
-	                                </a>
-	                            </p>
-	                        </div>
-	                        <div className="column is-5 is-offset-1">
-	                            <figure className="image is-4by3">
-	                                <img src="assets/images/illustrations/worker.svg" alt="Description" />
-	                            </figure>
-	                        </div>
+	                    <div className="columns is-centered">
+
 	        
 	                    </div>
 	                </div>
@@ -108,15 +93,7 @@ class Hero extends Component {
 	            
 	            <div className="hero-foot mb-20">
 	                <div className="container">
-	                    <div className="tabs is-centered">
-	                        <ul>
-	                            <li><a><img className="partner-logo" src="assets/images/logos/clients/systek.svg" /></a></li>
-	                            <li><a><img className="partner-logo" src="assets/images/logos/clients/tribe.svg" /></a></li>
-	                            <li><a><img className="partner-logo" src="assets/images/logos/clients/kromo.svg" /></a></li>
-	                            <li><a><img className="partner-logo" src="assets/images/logos/clients/infinite.svg" /></a></li>
-	                            <li><a><img className="partner-logo" src="assets/images/logos/clients/gutwork.svg" /></a></li>
-	                        </ul>
-	                    </div>
+	                    <p className="title">Powered by</p>
 	                </div>
 	            </div>
 	        </section>
